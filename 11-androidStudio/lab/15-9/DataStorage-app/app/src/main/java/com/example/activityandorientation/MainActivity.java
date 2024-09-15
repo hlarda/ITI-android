d299645e3d696c9c6468fd5a966452c8ac4a7f3c
hlarda <halareda63@gmail.com>
2024-09-15 16:25:03 +0300

    androidLab: DataStorage- without SQL

---
+++ 11-androidStudio/lab/15-9/DataStorage-app/app/src/main/java/com/example/activityandorientation/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.activityandorientation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String PHONE = "phone";
+    public static final String MESSAGE = "message";
+
+    Button nextBtn, closeBtn;
+    EditText editPhoneTxt, editMsgTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        nextBtn = findViewById(R.id.nextBtn);
+        closeBtn = findViewById(R.id.closeBtn);
+
+        editPhoneTxt = findViewById(R.id.editPhoneTxt);
+        editMsgTxt = findViewById(R.id.editMsgTxt);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone = editPhoneTxt.getText().toString();
+                String message = editMsgTxt.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this, nextActivity.class);
+                intent.putExtra(PHONE, phone);
+                intent.putExtra(MESSAGE, message);
+                startActivity(intent);
+            }
+        });
+        closeBtn.setOnClickListener(view -> moveTaskToBack(true));
+    }
+}
---
+++ 11-androidStudio/lab/15-9/DataStorage-app/app/src/main/java/com/example/activityandorientation/nextActivity.java
@@ -0,0 +1,124 @@
+package com.example.activityandorientation;
+
+import static com.example.activityandorientation.R.id.rSharedPrefBtn;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class nextActivity extends AppCompatActivity {
+    private static final String PREF_NAME = "PREF_NAME";
+    private static final String INTERNAL_FILE = "INTERNAL_FILE";
+
+    Button backBtn;
+    Button rSharedPrefBtn, wSharedPrefBtn, rInternalStorage, wInternalStorage;
+    TextView phoneView, msgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_next);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        backBtn         = findViewById(R.id.backBtn);
+        rSharedPrefBtn  = findViewById(R.id.rSharedPrefBtn);
+        wSharedPrefBtn  = findViewById(R.id.wSharedPrefBtn);
+        rInternalStorage= findViewById(R.id.rInternalStorage);
+        wInternalStorage= findViewById(R.id.wInternalStorage);
+
+        phoneView       = findViewById(R.id.phoneView);
+        msgView         = findViewById(R.id.msgView);
+
+        String phone = getIntent().getStringExtra(MainActivity.PHONE);
+        String message = getIntent().getStringExtra(MainActivity.MESSAGE);
+
+        phoneView.setText(phone);
+        msgView.setText(message);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        wSharedPrefBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(MainActivity.PHONE,phone);
+                editor.putString(MainActivity.MESSAGE,message);
+                editor.apply();
+                phoneView.setText("");
+                msgView.setText("");
+            }
+        });
+        rSharedPrefBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+                phoneView.setText(sharedPreferences.getString(MainActivity.PHONE,"N/A"));
+                msgView.setText(sharedPreferences.getString(MainActivity.MESSAGE,"N/A"));
+            }
+        });
+        wInternalStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream fos = openFileOutput(INTERNAL_FILE, Context.MODE_PRIVATE);
+                    DataOutputStream dos = new DataOutputStream(fos);
+                    dos.writeUTF(phone);
+                    dos.writeUTF(message);
+                    phoneView.setText("");
+                    msgView.setText("");
+                    fos.close();
+                    dos.close();
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(nextActivity.this, "Couldn't create file", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(nextActivity.this, "Couldn't write to the file", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        rInternalStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileInputStream fis = openFileInput(INTERNAL_FILE);
+                    DataInputStream dis = new DataInputStream(fis);
+                    phoneView.setText(dis.readUTF());
+                    msgView.setText(dis.readUTF());
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(nextActivity.this, "Couldn't create file", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    Toast.makeText(nextActivity.this, "Couldn't read from the file", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
