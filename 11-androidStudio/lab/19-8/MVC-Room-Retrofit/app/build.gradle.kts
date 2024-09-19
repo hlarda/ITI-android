55c6f188a8e65a21def14c6c2386916f74f9ceed
hlarda <halareda63@gmail.com>
2024-09-19 13:26:06 +0300

    androidLab: MVC-Room-Retrofit -> Added dependencies and network permission

--- 11-androidStudio/lab/19-8/MVC-Room-Retrofit/app/build.gradle.kts
+++ 11-androidStudio/lab/19-8/MVC-Room-Retrofit/app/build.gradle.kts
@@ -40,4 +40,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Retrofit and Gradle
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.google.code.gson:gson:2.8.9")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
+    // Room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
 }
